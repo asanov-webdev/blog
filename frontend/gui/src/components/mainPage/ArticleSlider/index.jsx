cc5025ddd6d2ecef9152772f4d3e63ad27086b59
Asanov Arthur <asanovarthur1@gmail.com>
2020-02-26 15:14:30 +0300

    Finished work on slider images display

--- frontend/gui/src/components/mainPage/ArticleSlider/index.jsx
+++ frontend/gui/src/components/mainPage/ArticleSlider/index.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { fetchLatestArticles } from "../../../api";
 import Article from "./Article";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
 import "./styles.css";
 import one from "../../../assets/1.jpg";
 import two from "../../../assets/2.jpg";
@@ -20,65 +19,32 @@ export const ArticleSlider = () => {
   }, []);
 
   const settings = {
-    dots: true,
-    infinite: false,
-    // variableWidth: false,
-    outerEdgeLimit: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    initialSlide: 0.175
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    variableWidth: true,
+    infinite: true,
+    arrows: true,
+    speed: 600
   };
 
   return (
-    <Slider {...settings} className='my-slider'>
-      <div className='slide-div'>
-        <img className="slide-img" src={one} />
-      </div>
-      <div className='slide-div'>
-        <img className="slide-img" src={two} />
-      </div>
-      <div className='slide-div'>
-        <img className="slide-img" src={three} />
-      </div>
-      <div className='slide-div'>
-        <img className="slide-img" src={four} />
-      </div>
-    </Slider>
+    <div className="container">
+      <Slider {...settings}>
+        <div className="slide-div">
+          <img className="slide-img" src={one} />
+        </div>
+        <div className="slide-div">
+          <img className="slide-img" src={two} />
+        </div>
+        <div className="slide-div">
+          <img className="slide-img" src={three} />
+        </div>
+        <div className="slide-div">
+          <img className="slide-img" src={four} />
+        </div>
+      </Slider>
+    </div>
   );
-
-  // return (
-  //   <Carousel
-  //     showArrows={false}
-  //     showStatus={false}
-  //     showIndicators={false}
-  //     showThumbs={false}
-  //     swipeScrollTolerance={10}
-  //     useKeyboardArrows
-  //     infiniteLoop
-  //     emulateTouch
-  //     centerMode
-  //     className="carousel"
-  //   >
-  //     <div>
-  //       <img className='slide-img' src={one} />
-  //       <p className='slide-p'>Test123</p>
-  //     </div>
-  //     <div>
-  //       <img className='slide-img' src={two} />
-  //       <p>2</p>
-  //     </div>
-  //     <div>
-  //       <img className='slide-img' src={three} />
-  //       <p>3</p>
-  //     </div>
-  //     <div>
-  //       <img className='slide-img' src={four} />
-  //       <p>4</p>
-  //     </div>
-  //   </Carousel>
-  // );
 };
 
 export default ArticleSlider;
