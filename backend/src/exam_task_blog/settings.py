339230f5461f8dbcbd374056060f5001292ece55
Arthur Asanov <asanovarthur1@gmail.com>
2020-05-25 09:34:22 +0300

    Removed redundant migrations, added requirements file

--- backend/src/exam_task_blog/settings.py
+++ backend/src/exam_task_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '19q9(e__%2gjejgaz49p3bvx%_(6^2xv=^+1crvhij1xee6k)w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
